521fc9449ed6df259cd7f4568996d1aa796498b9
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-01 07:39:23 +0000

    Add Classes to define all the Enpoint request and response objects.
    This is the API contract reusable between client and Server.
    The classes are all empty to start we will populate as we go.

---
+++ src/eShopOnBlazorWasm/Source/Api/Features/Catalog/CatalogItem/GetCatalogItemsPaginated/GetCatalogItemsPaginatedRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace eShopOnBlazorWasm.Features.Catalog
+{
+  using eShopOnBlazorWasm.Features.Bases;
+  using MediatR;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  public class GetCatalogItemsPaginatedRequest : BaseRequest, IRequest<GetCatalogItemsPaginatedResponse>
+  {
+    
+  }
+}
