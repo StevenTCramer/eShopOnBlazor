f94a356e4f1927fdaf80030be7166f2721c3baf0
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-01 09:03:59 +0000

    Missed a Response inheritance

--- src/eShopOnBlazorWasm/Source/Api/Features/Catalog/CatalogType/GetCatalogTypes/GetCatalogTypesResponse.cs
+++ src/eShopOnBlazorWasm/Source/Api/Features/Catalog/CatalogType/GetCatalogTypes/GetCatalogTypesResponse.cs
@@ -1,11 +1,12 @@
 ﻿namespace eShopOnBlazorWasm.Features.Catalog
 {
+  using eShopOnBlazorWasm.Features.Bases;
   using System;
   using System.Collections.Generic;
   using System.Linq;
   using System.Text;
   using System.Threading.Tasks;
-  public class GetCatalogTypesResponse
+  public class GetCatalogTypesResponse: BaseResponse
   {
     
   }
